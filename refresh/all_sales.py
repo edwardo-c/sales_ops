6e94c8bfd97b6df457befa06cafa2543e279deac
EdwardoCallejas <e.callejas14@yahoo.com>
2025-08-12 14:35:41 -0500

    Moved StatusReport Data refresh out of pipeline
    moved into separate class to be ran as part of the pipeline, instead of the entire process within the pipeline. Next build ReportBuilder class to query and connect to JSON

--- refresh/all_sales.py
+++ refresh/all_sales.py
@@ -1,36 +1,76 @@
-from data_toolkit.logger import logger
-from config.paths import ALLSALES_2025, ALLSALES_2024, DATABASE, ALL_SALES_LOG
-from data_toolkit.refresh_logger.refresh_logger import RefreshLogger
 from data_toolkit.base_loader import BaseLoader
-from data_toolkit.plans.plans import PLAN_DIRECT_SALES_ACU
-from data_toolkit.cleaning.cleaner import Cleaner
-from data_toolkit.exporter.exporter import Exporter
-from pathlib import Path
+from data_toolkit.plans.file_reader_details import STATUS_REPORT_SALES_2024, STATUS_REPORT_SALES_2025
+from config.paths import DATABASE
+from data_toolkit.dataplan import transforms  # noqa: triggers auto-registration
+from data_toolkit.dataplan.registry import GLOBAL_REGISTRY, build_registry
+from data_toolkit.dataplan.executor import DataPlanExecutor
+from data_toolkit.exporter import Exporter
+import pandas as pd
+
+project_registry = build_registry(
+    include=["rename_columns", "static_value",]
+)
+
+class RefreshAllSales:
+    def __init__(self):
+        self.dirty_data = None
+        self.all_sales = None
+
+    def run(self):
+        self.dirty_data = self._get_dirty_data()
+        self.all_sales = self._execute_clean_plan()
+        self._import_into_mssql()
+        print(self.all_sales.head())
+        return self.all_sales  # handy for tests or chaining
+
+    def _get_dirty_data(self):
+        base_loader = BaseLoader(
+            STATUS_REPORT_SALES_2024,
+            STATUS_REPORT_SALES_2025,
+        )
+        return base_loader.data  # {'alias': df}
+
+    def _execute_clean_plan(self):
+        plans = {
+            "allsales_2025": [
+                {"op": "rename_columns", "args": {
+                    "Customer Account Number": "acct_num",
+                    "Amount": "amount",
+                    "Classification(Sales Category)": "product_category",
+                    "Invoice Date": "invoice_date",
+                    "Inventory CD": "part_number",
+                }},
+                {"op": "static_value", "args": {"value": 2025, "col_name": "invoice_year"}},
+            ],
+            "allsales_2024": [
+                {"op": "rename_columns", "args": {
+                    "Account No.": "acct_num",
+                    "Amount": "amount",
+                    "Category": "product_category",
+                    "Invoice Date": "invoice_date",
+                    "Inventory CD": "part_number",
+                }},
+                {"op": "static_value", "args": {"value": 2024, "col_name": "invoice_year"}},
+            ],
+        }
+
+        executor = DataPlanExecutor(self.dirty_data, plans, registry=project_registry, strict=False)
+        all_sales_dict = {'all_sales': pd.concat(executor.processed_data.values())}
+        # TODO: Execute a clean plan on all sales
+        return all_sales_dict['all_sales']
+
+    def _import_into_mssql(self):
+        e = Exporter(self.all_sales)
+        e.to_sql(
+            table_name="status_report_all_sales",
+            engine_url=DATABASE,
+            if_exists="replace"
+        )
 
 def main():
-    logger.info(f"running refresh.all_sales.py")
-
-    try: 
-        file_map = [ALLSALES_2025, ALLSALES_2024]
-        loader = BaseLoader(file_map)
-        loader.load_data()
-
-        cleaner = Cleaner(loader.data, PLAN_DIRECT_SALES_ACU, append=True)
-        cleaner.clean()
-
-        exporter = Exporter(cleaner.output)
-        exporter.to_sql(DATABASE, 'allsales')
-
-        logger.info(f"all_sales | status=success | rows={len(cleaner.output)}")
-
-    except Exception as e:
-        logger.info('all_sales', 'failed', notes={e})
+    print("running refresh.all_sales")
+    RefreshAllSales().run()
+    print("refresh.all_sales complete")
 
 if __name__ == "__main__":
     main()
-
-
-'''transformer class ideas:
-unpivot
-break apart into separate groups with group by
-'''
